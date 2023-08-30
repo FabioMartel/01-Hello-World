03110e873a0700b1c296398a4fbcb435009cb1a6
FabioMartel <fabio.martel@vallegrande.edu.pe>
2023-08-30 16:13:02 -0500

    Hello World - app counter

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -6,20 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./counter.component.css'],
 })
 export class CounterComponent {
-  counter = 0;
-  activeDecrease = false;
+  counter = 0;             // Valor inicial del contador
+  activeDecrease = false;  // Variable para controlar la activación/desactivación de la disminución
 
   increase() {
-    // this.counter = this.counter + 1;
-    this.counter++;
+    this.counter++;  // Incrementa el contador en 1 al presionar el botón "+1"
   }
 
   decrease() {
-    // this.counter = this.counter - 1;
-    this.counter--;
+    if (this.counter > 0) {
+      this.counter--;  // Disminuye el contador en 1, si es mayor que 0
+    }
+
+    if (this.counter === 0) {
+      this.activeDecrease = true;  // Activa el bloqueo del botón de disminución cuando el contador llega a 0
+    }
   }
 
   inactiveDecrease() {
-    this.activeDecrease = !this.activeDecrease;
+    this.activeDecrease = !this.activeDecrease;  // Cambia el estado de la activación/desactivación de la disminución
   }
 }
